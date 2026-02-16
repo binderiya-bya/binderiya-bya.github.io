d14e3884af9046ba201cd7817ac26e4394808752
binderiya-bya <bb3174a@american.edu>
2026-02-16 22:20:57 +0000

    Deploying to gh-pages from @ binderiya-bya/binderiya-bya.github.io@27879711d1990430fca9d835c03b0ac7aa261405 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-consultancy",
-          title: "consultancy",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/consultancy/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
